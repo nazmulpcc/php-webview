f30445a31b59d6ea7301e061a01a7332bac46acb
Nazmul Alam <n@zmul.dev>
2024-03-11 10:59:23 +0600

    test webview function

--- webview.c
+++ webview.c
@@ -5,6 +5,7 @@
 #endif
 
 #include "php.h"
+#include "webview.h"
 #include "ext/standard/info.h"
 #include "php_webview.h"
 #include "webview_arginfo.h"
@@ -16,10 +17,29 @@
 	ZEND_PARSE_PARAMETERS_END()
 #endif
 
-static zend_class_entry *webview_class_entry = NULL;
+static zend_object_handlers webview_object_handlers;
+
+typedef struct php_webview_t
+{
+	webview_t native;
+	zend_object std;
+} php_webview_t;
+
+#define Z_WEBVIEW_P(zv) \
+	((php_webview_t *)((char *)(Z_OBJ_P(zv)) - XtOffsetOf(php_webview_t, std)))
+
+zend_object *webview_new(zend_class_entry *ce)
+{
+	php_webview_t *wv = zend_object_alloc(sizeof(php_webview_t), ce);
+
+	zend_object_std_init(&wv->std, ce);
+	wv->std.handlers = &webview_object_handlers;
+	return &wv->std;
+}
 
 PHP_METHOD(Webview, __construct)
 {
+	php_webview_t *container = Z_WEBVIEW_P(ZEND_THIS);
 	zend_long debug = 0;
 
 	ZEND_PARSE_PARAMETERS_START(0, 1)
@@ -32,17 +52,24 @@ PHP_METHOD(Webview, __construct)
 		zend_update_property_long(Z_OBJCE_P(ZEND_THIS), Z_OBJ_P(ZEND_THIS),
 								  "debug", sizeof("debug") - 1, debug);
 	}
+
+	container->native = webview_create(debug, NULL);
 }
 
 PHP_METHOD(Webview, hello)
 {
 	zval *salutation;
+	php_webview_t *container = Z_WEBVIEW_P(ZEND_THIS);
 
 	ZEND_PARSE_PARAMETERS_START(1, 1)
 	Z_PARAM_ZVAL(salutation)
 	ZEND_PARSE_PARAMETERS_END();
 
+	webview_set_title(container->native, Z_STRVAL(*salutation));
+	webview_set_html(container->native, "Hello World");
+
 	zend_print_zval(salutation, 0);
+	webview_run(container->native);
 }
 
 /* {{{ void test1() */
